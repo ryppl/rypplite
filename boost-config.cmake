323f1bbe34565857516a6b39492427d7aa0e946e
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-05-17 11:53:00 +0200

    add 'update' target

--- boost-config.cmake
+++ boost-config.cmake
@@ -20,10 +20,11 @@ set(Boost_RESOURCE_PATH "${RYPPL_DEPENDENCY_DIR}/cmake/resources")
 set(Boost_USE_FILE "${RYPPL_DEPENDENCY_DIR}/cmake/modules/UseBoost.cmake")
 set(Boost_DEV_FILE "${RYPPL_DEPENDENCY_DIR}/cmake/modules/UseBoostDev.cmake")
 
+find_package(Git REQUIRED)
+
 function(ryppl_fetch_dependency name)
   set(url "git://github.com/boost-lib/${name}.git")
   if(NOT EXISTS "${RYPPL_DEPENDENCY_DIR}/${name}")
-    find_package(Git REQUIRED)
     file(MAKE_DIRECTORY "${RYPPL_DEPENDENCY_DIR}")
     execute_process(
       COMMAND "${GIT_EXECUTABLE}" clone ${url} ${name}
@@ -40,20 +41,28 @@ if(NOT DEFINED RYPPL_DEPENDENCY_LEVEL)
   set(RYPPL_DEPENDENCY_LEVEL 0 CACHE INTERNAL "" FORCE)
 endif(NOT DEFINED RYPPL_DEPENDENCY_LEVEL)
 
+if(NOT TARGET update)
+  add_custom_target(update)
+endif(NOT TARGET update)
+
 math(EXPR level "${RYPPL_DEPENDENCY_LEVEL} + 1")
 set(RYPPL_DEPENDENCY_LEVEL ${level} CACHE INTERNAL "" FORCE)
 
 foreach(component cmake ${Boost_FIND_COMPONENTS})
-  ryppl_fetch_dependency(${component})
-
   list(FIND RYPPL_COMPONENTS ${component} index)
   if(index EQUAL -1)
+    ryppl_fetch_dependency(${component})
     list(APPEND RYPPL_COMPONENTS ${component})
     add_subdirectory(
       "${RYPPL_DEPENDENCY_DIR}/${component}"
       "${RYPPL_DEPENDENCY_DIR}-build/${component}"
       EXCLUDE_FROM_ALL
       )
+    add_custom_target(${component}-update
+      COMMAND "${GIT_EXECUTABLE}" pull
+      WORKING_DIRECTORY "${RYPPL_DEPENDENCY_DIR}/${component}"
+      )
+    add_dependencies(update ${component}-update)
     if(RYPPL_DEPENDENCY_LEVEL GREATER 1)
       set(RYPPL_COMPONENTS ${RYPPL_COMPONENTS} PARENT_SCOPE)
     endif(RYPPL_DEPENDENCY_LEVEL GREATER 1)
@@ -62,4 +71,3 @@ endforeach(component)
 
 math(EXPR level "${RYPPL_DEPENDENCY_LEVEL} - 1")
 set(RYPPL_DEPENDENCY_LEVEL ${level} CACHE INTERNAL "" FORCE)
-
